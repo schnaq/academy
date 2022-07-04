1e32d56be40088f2e1d0bac4b9165b3bd72c5619
Christian Meter <cmeter@googlemail.com>
2022-07-04 09:38:48 +0200

    Add title to config

--- src/main/schnaq/academy/config.cljs
+++ src/main/schnaq/academy/config.cljs
@@ -1,5 +1,8 @@
 (ns schnaq.academy.config)
 
+(def application-name
+  "schnaq academy")
+
 (def default-share-hash
   "Point to a default schnaq. In this case the FAQ production schnaq."
   "e8f54922-0d88-4953-8f43-ddc819d7f201")
