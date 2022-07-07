248d50805ff5a91e6b3202dc88bd88039cd5529b
Christian Meter <cmeter@googlemail.com>
2022-07-07 15:09:01 +0200

    Add icons

--- src/main/schnaq/academy/pages/index.cljs
+++ src/main/schnaq/academy/pages/index.cljs
@@ -1,5 +1,6 @@
 (ns schnaq.academy.pages.index
-  (:require [re-frame.core :as rf]
+  (:require ["@heroicons/react/outline" :refer [PresentationChartBarIcon GlobeIcon]]
+            [re-frame.core :as rf]
             [reitit.frontend.easy :as rfe]
             [schnaq.academy.components :refer [card]]
             [schnaq.academy.pages.base :refer [base]]
@@ -17,12 +18,12 @@
       [:p "schnaq auf einer eigenen Plattform zu betreiben ist zwar schön, aber noch schöner ist es, wenn wir schnaq direkt zu dir auf deine Webseiten und Anwendungen bringen. Lerne hier, wie das funktionieren kann."]
       [:div.grid.md:grid-cols-3
        [card
-        "iFrames"
+        [:<> [:> GlobeIcon {:class "h-7 inline mr-2"}] "iFrames"]
         "schnaq kann in beliebige Webkontexte per iFrame eingebunden werden."
         "Weiterlesen"
         (rfe/href :routes/embedding {} query-parameters)]
        [card
-        "Powerpoint"
+        [:<> [:> PresentationChartBarIcon {:class "h-7 inline mr-2"}] "Powerpoint"]
         "Live Fragen in deinen Folien? Wir zeigen dir wie das mit Powerpoint möglich ist!"
         "Weiterlesen"
         (rfe/href :routes.embedding/powerpoint {} query-parameters)]]]]))
