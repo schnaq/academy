2527637a81d647faca2f5d895d82b66a80eb77ab
Christian Meter <cmeter@googlemail.com>
2022-07-13 17:18:55 +0200

    Add academy url

--- src/main/schnaq/academy/config.cljs
+++ src/main/schnaq/academy/config.cljs
@@ -3,6 +3,8 @@
 (def application-name
   "schnaq academy")
 
+(goog-define academy-url "http://localhost:8000")
+
 (goog-define frontend-url "https://app.staging.schnaq.com")
 (goog-define default-share-hash "6586e787-8704-4b4b-9221-5821d15626b5")
 
