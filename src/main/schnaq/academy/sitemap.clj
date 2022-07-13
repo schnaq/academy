7f7c5d9010066940e833d939efd0cee737f6a990
Christian Meter <cmeter@googlemail.com>
2022-07-13 21:04:06 +0200

    Use sitemap lib

--- src/main/schnaq/academy/sitemap.clj
+++ src/main/schnaq/academy/sitemap.clj
@@ -1,10 +1,10 @@
 (ns schnaq.academy.sitemap
-  (:require [clojure.data.xml :as xml]
-            [clojure.spec.alpha :as s]
+  (:require [clojure.spec.alpha :as s]
             [com.fulcrologic.guardrails.core :refer [=> >defn]]
             [reitit.core :as rc]
             [schnaq.academy.config :as config]
-            [schnaq.academy.routes :as routes]))
+            [schnaq.academy.routes :as routes]
+            [sitemap.core :refer [generate-sitemap]]))
 
 (defn all-routes
   "Read all route-paths from the router."
@@ -17,18 +17,12 @@
   [(s/coll-of string?) => (s/coll-of string?)]
   (map #(format "%s%s" config/academy-url %) routes))
 
-(defn url->xml
-  "Build XML representation of a url."
-  [url]
-  (xml/element :url {}
-               (xml/element :loc {} url)))
-
 (defn build-sitemap-xml
   "Takes a reitit router and builds a xml sitemap."
   [router]
-  (let [urls (map url->xml (absolute-urls (all-routes router)))]
-    (xml/emit-str (xml/element* :urlset {:xmlns "http://www.w3.org/1999/xhtml"}
-                                urls))))
+  (let [urls (for [url (absolute-urls (all-routes router))]
+               {:loc url})]
+    (generate-sitemap urls)))
 
 (defn generate-sitemap! []
   (spit "resources/public/sitemap.xml"
