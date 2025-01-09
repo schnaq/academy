5e7ffc1d83011c48e7618fa0aa88b92fe06d05d2
Christian Meter <cmeter@googlemail.com>
2025-01-09 10:02:23 +0100

    chore: Update favicon URLs and logo source in HTML and Clojure files

--- src/main/schnaq/academy/pages/base.cljs
+++ src/main/schnaq/academy/pages/base.cljs
@@ -1,55 +1,55 @@
 (ns schnaq.academy.pages.base
-  (:require ["@heroicons/react/solid" :refer [MoonIcon SunIcon]]
-            [re-frame.core :as rf]
-            [reitit.frontend.easy :as rfe]
-            [schnaq.academy.config :as config]
-            [schnaq.academy.utils :as utils]))
+    (:require ["@heroicons/react/solid" :refer [MoonIcon SunIcon]]
+              [re-frame.core :as rf]
+              [reitit.frontend.easy :as rfe]
+              [schnaq.academy.config :as config]
+              [schnaq.academy.utils :as utils]))
 
 (defn- header
-  "Define the academy header."
-  []
-  (let [dark-mode? @(rf/subscribe [:dark-mode?])
-        query-parameters @(rf/subscribe [:navigation/query-parameters])]
-    [:nav.flex.bg-blue.dark:bg-blue-dark.p-6.text-white
-     [:div.flex.items-center.flex-no-shrink.mr-6.cursor-pointer
-      {:on-click #(rf/dispatch [:navigation/navigate :routes/start])}
-      [:img.h-8.pr-2 {:src "https://s3.schnaq.com/schnaq-common/logos/schnaqqifant_white.webp"}]
-      [:span.font-semibold.text-xl.tracking-tight
-       config/application-name]]
-     [:a.header-link {:href (rfe/href :routes/start {} query-parameters)} "Start"]
-     [:div.ml-auto]
-     [:> (if dark-mode? MoonIcon SunIcon)
-      {:class "h-7 my-auto pr-3 cursor-pointer"
-       :on-click #(rf/dispatch [:dark-mode/toggle])}]
-     [:a.header-link-external
-      {:href "https://schnaq.com"} "schnaq.com"]]))
+       "Define the academy header."
+       []
+       (let [dark-mode? @(rf/subscribe [:dark-mode?])
+             query-parameters @(rf/subscribe [:navigation/query-parameters])]
+            [:nav.flex.bg-blue.dark:bg-blue-dark.p-6.text-white
+             [:div.flex.items-center.flex-no-shrink.mr-6.cursor-pointer
+              {:on-click #(rf/dispatch [:navigation/navigate :routes/start])}
+              [:img.h-8.pr-2 {:src "https://snq-common.s3.nl-ams.scw.cloud/logos/schnaqqifant_white.webp"}]
+              [:span.font-semibold.text-xl.tracking-tight
+               config/application-name]]
+             [:a.header-link {:href (rfe/href :routes/start {} query-parameters)} "Start"]
+             [:div.ml-auto]
+             [:> (if dark-mode? MoonIcon SunIcon)
+              {:class "h-7 my-auto pr-3 cursor-pointer"
+               :on-click #(rf/dispatch [:dark-mode/toggle])}]
+             [:a.header-link-external
+              {:href "https://schnaq.com"} "schnaq.com"]]))
 
 (defn- footer
-  "Define the academy footer."
-  []
-  [:footer
-   [:nav.flex.bg-blue-dark.p-6.text-white.mt-5
-    [:div.flex.items-center.flex-no-shrink.mr-6
-     [:img.h-8.pr-2 {:src "https://s3.schnaq.com/schnaq-common/logos/schnaqqifant_white.webp"}]
-     [:span.font-semibold.text-xl.tracking-tight config/application-name]]
-    [:div.ml-auto]
-    [:a.text-white.mr-3 {:target :_blank :href "https://schnaq.com/de/legal-note"} "Impressum"]
-    [:a.text-white {:target :_blank :href "https://schnaq.com/de/privacy"} "Datenschutz"]]])
+       "Define the academy footer."
+       []
+       [:footer
+        [:nav.flex.bg-blue-dark.p-6.text-white.mt-5
+         [:div.flex.items-center.flex-no-shrink.mr-6
+          [:img.h-8.pr-2 {:src "https://snq-common.s3.nl-ams.scw.cloud/logos/schnaqqifant_white.webp"}]
+          [:span.font-semibold.text-xl.tracking-tight config/application-name]]
+         [:div.ml-auto]
+         [:a.text-white.mr-3 {:target :_blank :href "https://schnaq.com/de/legal-note"} "Impressum"]
+         [:a.text-white {:target :_blank :href "https://schnaq.com/de/privacy"} "Datenschutz"]]])
 
 ;; -----------------------------------------------------------------------------
 
 (defn root
-  "Root view to initialize and render the application."
-  []
-  (if-let [current-view @(rf/subscribe [:navigation/current-view])]
-    [current-view]
-    [:div]))
+      "Root view to initialize and render the application."
+      []
+      (if-let [current-view @(rf/subscribe [:navigation/current-view])]
+              [current-view]
+              [:div]))
 
 (defn base [{:page/keys [title description]} body]
-  (utils/set-website-title! title)
-  (utils/set-website-description! description)
-  [:main
-   [:div.dark:bg-gray-700.dark:text-white.flex.flex-col.min-h-screen
-    [header]
-    [:div.container.mx-auto.px-3.pt-3.grow body]
-    [footer]]])
+      (utils/set-website-title! title)
+      (utils/set-website-description! description)
+      [:main
+       [:div.dark:bg-gray-700.dark:text-white.flex.flex-col.min-h-screen
+        [header]
+        [:div.container.mx-auto.px-3.pt-3.grow body]
+        [footer]]])
